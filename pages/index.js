c93e311e700a8c8093b5daecba9b21ebc5b0719a
arbin <arbin@xceltrip.com>
2022-04-06 15:57:34 +0530

    readme

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import HomePage from "../src/index";
 
-export default function Home() {
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Arbin Shrestha</title>
-				<meta name="description" content="Arbin Shrestha" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-
-			<main className={styles.main}>
-				<h1 className={styles.title}>Hello World!</h1>
-			</main>
-
-			<footer className={styles.footer}>
-				<a
-					href="https://instagram.com/arbeen_stha"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Arbin © 2022
-				</a>
-			</footer>
-		</div>
-	);
-}
+const HomePage = () => <HomePage />;
+export default HomePage;
