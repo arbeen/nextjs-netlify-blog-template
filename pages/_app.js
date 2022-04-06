af159709fa2ef9721c62f76fc988560796f615bc
arbin <arbin@xceltrip.com>
2022-04-07 01:07:12 +0530

    mantine ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,45 @@
-import '../styles/globals.css'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider, AppShell, Header } from "@mantine/core";
+import { DoubleNavbar } from "../src/layout/sidebar";
+import { HeaderSearch } from "../src/layout/header";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+	return (
+		<>
+			<Head>
+				<title>Page title</title>
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width"
+				/>
+			</Head>
+
+			<MantineProvider
+				withGlobalStyles
+				withNormalizeCSS
+				theme={{
+					/** Put your mantine theme override here */
+					colorScheme: "light",
+				}}
+			>
+				<AppShell
+					padding="md"
+					navbar={<DoubleNavbar />}
+					header={<HeaderSearch />}
+					styles={(theme) => ({
+						main: {
+							backgroundColor:
+								theme.colorScheme === "dark"
+									? theme.colors.dark[8]
+									: theme.colors.gray[0],
+						},
+					})}
+				>
+					{/* Your application here */}
+					<Component {...pageProps} />
+				</AppShell>
+			</MantineProvider>
+		</>
+	);
 }
-
-export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import HomePage from "../src/index";
+import { fetchAPI } from "../lib/api";
 
-const Home = () => <HomePage />;
+const Home = ({ faqs }) => <HomePage faqs={faqs} />;
+
+export async function getStaticProps() {
+	// Run API calls in parallel
+	const faqsRes = await fetchAPI("/faqs");
+
+	return {
+		props: {
+			faqs: faqsRes.data,
+		},
+		revalidate: 1,
+	};
+}
 export default Home;
