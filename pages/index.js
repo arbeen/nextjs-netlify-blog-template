f81fd55fd4ec8fa72e846b5e7ba033adafd7cf72
arbin <arbin@xceltrip.com>
2022-04-06 15:59:54 +0530

    readme

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import HomePage from "../src/index";
 
-const HomePage = () => <HomePage />;
-export default HomePage;
+const Home = () => <HomePage />;
+export default Home;
